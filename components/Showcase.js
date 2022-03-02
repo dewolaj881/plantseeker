368385d32d8921e959c66fe20b7c936f6de50ab1
dewolaj881 <dewolaj881@gmail.com>
2022-03-03 00:20:30 +0100

    add: navbar, showcase & styles

---
+++ components/Showcase.js
@@ -0,0 +1,9 @@
+
+export default function Showcase() {
+  return (
+    <div className="bg-showcase bg-cover bg-no-repeat bg-center absolute">
+      <h1 className="text-showcase relative mt-48">Take care of Your plants with us.</h1>
+      <p>We are the biggest library storing information about plants.</p>
+    </div>
+  )
+}
