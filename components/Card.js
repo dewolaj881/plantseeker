e14f638db6acb1c63b213870f4fa29fa5651ba8c
dewolaj881 <dewolaj881@gmail.com>
2022-03-15 00:27:10 +0100

    mouse pointer on card

--- components/Card.js
+++ components/Card.js
@@ -4,8 +4,8 @@ export default function Card({ plant = {} }) {
   const { description, englishName, family, id, img, latinName, origin, polishName, species } = plant; // Takie pola są w obecnym API, każde może być nullem, id jest liczbą, reszta string
   console.log(plant);
   return (
-    <Link href={"/search/plant_" + plant.id} >
-      <div>
+    <Link href={"/search/plant_" + plant.id}>
+      <div className="cardLink">
         <img className="w-56 h-42 rounded-t-sm" src={img || 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'} alt={polishName} />
         <div className="w-56 bg-secondary rounded-b-sm text-white p-4">
           <h1 className="text-center"><b>{polishName}</b> {latinName && `(${latinName})`}</h1>
