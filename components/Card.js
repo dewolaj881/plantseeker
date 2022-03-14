97e412d6e42e5939945c868c8bf634f7703a00c3
dewolaj881 <dewolaj881@gmail.com>
2022-03-14 20:47:44 +0100

    Change: links to plants by id

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,20 @@
-export default function Card({plant = {}}) {
-  const {description, englishName, family, id, img, latinName, origin, polishName, species} = plant; // Takie pola są w obecnym API, każde może być nullem, id jest liczbą, reszta string
+import Link from "next/link";
+
+export default function Card({ plant = {} }) {
+  const { description, englishName, family, id, img, latinName, origin, polishName, species } = plant; // Takie pola są w obecnym API, każde może być nullem, id jest liczbą, reszta string
   console.log(plant);
   return (
-    <div>
-      <img className="w-56 h-42 rounded-t-sm" src={img || 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'} alt={polishName}/>
-      <div className="w-56 bg-secondary rounded-b-sm text-white p-4">
-        <h1 className="text-center"><b>{polishName}</b> {latinName && `(${latinName})`}</h1>
-        <p className="w-48 text-justify">{description}</p>
-        <div className="flex flex-col">
-          <h2 className="text-left bg-gray-600 w-max px-4 rounded-xl">{family || 'Nieznany'}</h2>
+    <Link href={"/search/plant_" + plant.id} >
+      <div>
+        <img className="w-56 h-42 rounded-t-sm" src={img || 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'} alt={polishName} />
+        <div className="w-56 bg-secondary rounded-b-sm text-white p-4">
+          <h1 className="text-center"><b>{polishName}</b> {latinName && `(${latinName})`}</h1>
+          <p className="w-48 text-justify">{description}</p>
+          <div className="flex flex-col">
+            <h2 className="text-left bg-gray-600 w-max px-4 rounded-xl">{family || 'Nieznany'}</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </Link >
   )
 }
