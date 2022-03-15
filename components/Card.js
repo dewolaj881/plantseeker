2339af3eff251566b33e7ec0086c22c3c370e67f
dewolaj881 <dewolaj881@gmail.com>
2022-03-15 10:56:39 +0100

    add cardInfo(not done)

--- components/Card.js
+++ components/Card.js
@@ -9,7 +9,7 @@ export default function Card({ plant = {} }) {
         <div>
           <img className="w-72 h-56 rounded-2xl mx-4" src={img || 'https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg'} alt={polishName} />
           <div className="text-xl w-72 bg-transparent text-white font-bold minusMargin">
-            <h1 className="text-center"><b>{/*polishName*/ "Magnolia"}</b> <span className="font-light">{"Magnolia L."}</span></h1>
+            <h1 className="text-center ml-6"><b>{/*polishName*/ "Magnolia"}</b> <span className="font-light">{"Magnolia L."}</span></h1>
           </div>
         </div>
       </div>
