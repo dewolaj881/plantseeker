80e42d4a8b40e43816d4a37c1d6eff7e3af81780
dewolaj881 <dewolaj881@gmail.com>
2022-03-15 14:13:44 +0100

    remove comments

--- components/Card.js
+++ components/Card.js
@@ -16,24 +16,3 @@ export default function Card({ plant = {} }) {
     </Link >
   )
 }
-
-// import Link from "next/link";
-
-// export default function Card({ plant = {} }) {
-//   const { description, englishName, family, id, img, latinName, origin, polishName, species } = plant; // Takie pola są w obecnym API, każde może być nullem, id jest liczbą, reszta string
-//   console.log(plant);
-//   return (
-//     <Link href={"/search/plant_" + plant.id}>
-//       <div className="cardLink">
-//         <img className="w-56 h-42 rounded-t-sm" src={img || 'https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg'} alt={polishName} />
-//         <div className="w-56 bg-secondary rounded-b-sm text-white p-4">
-//           <h1 className="text-center"><b>{polishName}</b> {latinName && `(${latinName})`}</h1>
-//           <p className="w-48 text-justify">{description}</p>
-//           <div className="flex flex-col">
-//             <h2 className="text-left bg-gray-600 w-max px-4 rounded-xl">{family || 'Nieznany'}</h2>
-//           </div>
-//         </div>
-//       </div>
-//     </Link >
-//   )
-// }
