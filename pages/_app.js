6dddff605ad67daf36b2c6ea9c5c0b19d10b3eb9
PanSzelesck <panszelescik@gmail.com>
2022-03-16 21:59:33 +0100

    Add Microsoft Clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '../tailwind.config';
+import Script from 'next/script.js';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import ReactGA from 'react-ga4';
 import useAnalytics from '../utils/useAnalytics.js';
@@ -12,9 +13,18 @@ function MyApp({Component, pageProps}) {
   useAnalytics();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Script>
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "b25aljhjzr");`}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
 
