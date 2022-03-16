c04850b8620468cc673553b72b6612830aa92e9d
PanSzelesck <panszelescik@gmail.com>
2022-03-16 22:07:34 +0100

    Partial revert

--- components/PlantImage.js
+++ components/PlantImage.js
@@ -1,5 +1,5 @@
 export default function PlantImage({src, alt}) {
   return (
-    <img height="128" width="128" src={src} alt={alt}/>
+    <img height="500" width="500" src={src} alt={alt}/>
   );
 }
