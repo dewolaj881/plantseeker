6dd1b219d6ca097440c8e7c4bfdad8456b214068
dewolaj881 <dewolaj881@gmail.com>
2022-03-17 20:21:24 +0100

    change padding

--- components/plant/Description.js
+++ components/plant/Description.js
@@ -1,5 +1,5 @@
-export default function Description({description}) {
+export default function Description({ description }) {
   return description && (
-    <div className="max-w-6xl text-center py-5 px-10 text-white text-justify">{description}</div>
+    <div className="max-w-6xl py-5 px-5 text-white text-justify">{description}</div>
   );
 }
