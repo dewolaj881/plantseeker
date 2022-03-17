c32cd4aa40acba4db65083187f26eedfcc9d239d
PanSzelesck <panszelescik@gmail.com>
2022-03-17 20:46:50 +0100

    Fix multiple Clarity scripts

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({Component, pageProps}) {
 
   return (
     <>
-      <Script>
+      <Script id="clarity">
         {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
