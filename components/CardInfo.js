0e8df4441c456d801ef0f370a65de76e0509bf8e
dewolaj881 <dewolaj881@gmail.com>
2022-03-17 10:11:59 +0100

    add styling

--- components/CardInfo.js
+++ components/CardInfo.js
@@ -56,15 +56,18 @@ export default function CardInfo({id}) {
   return (
     <>
       <HeadTitle title={polishName}/>
-      <div className="h-content flex flex-wrap justify-between">
-        <div className="p-10 ">
-          <h1 className="text-3xl text-white pb-10"><b>{polishName}</b> <LatinNameCardInfo latinName={latinName}/></h1>
-          <div className="flex space-x-4 flex-wrap">
+      <div className="h-content flex flex-col">
+        <div className="p-10">
+          <div className="flex flex-wrap">
             {(images || []).map((image) => <PlantImage key={image.img_name} src={getImageUrl(image)} alt={polishName}/>)}
-            <QRCode url={url} size="512"/>
           </div>
+          <h1 className="text-3xl text-white pt-10"><b>{polishName}</b> <LatinNameCardInfo latinName={latinName}/></h1>
         </div>
-        <Description description={description}/>
+        <Description description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin mollis tortor, et fermentum orci vestibulum sed. Nulla egestas vehicula sem, sit amet mattis nulla. Suspendisse ut sapien vel massa mollis gravida at in lorem. Nam et lobortis nunc, dapibus posuere ex. Donec sit amet mattis tortor. Donec imperdiet fermentum elit non feugiat. Praesent eget odio at elit tempor euismod tempor at nisl. Vestibulum ac vestibulum dui, eget bibendum lorem. Cras pretium, augue sit amet ultrices hendrerit, orci mi vehicula mauris, vel luctus purus nunc nec lectus. Integer hendrerit sit amet nunc a eleifend. Proin ac blandit augue. Duis sodales nisl vitae diam sodales auctor et a quam. Praesent sem sem, cursus vel faucibus at, pharetra eu turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."}/>
+        <div className="mt-24 text-white">
+          <h2>Drukuj kod QR</h2>
+          <QRCode url={url} size="512"/>
+        </div>
       </div>
     </>
   );
