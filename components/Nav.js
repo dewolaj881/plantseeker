cd9075365e1ab908950deb7ce81dde6364359826
PanSzelescik <panszelescik@gmail.com>
2022-03-17 08:12:27 +0100

    Change background color to fix search background color

--- components/Nav.js
+++ components/Nav.js
@@ -15,7 +15,7 @@ const navigation = [
 
 export default function Nav({ current }) {
   return (
-    <Disclosure as="nav" className="bg-main">
+    <Disclosure as="nav">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
