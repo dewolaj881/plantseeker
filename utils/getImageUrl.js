d7f33706468e251303b4ba6e62b390f232ce5e86
PanSzelesck <panszelescik@gmail.com>
2022-03-23 19:21:11 +0100

    Support other images with http or https

--- utils/getImageUrl.js
+++ utils/getImageUrl.js
@@ -1,6 +1,13 @@
 export default function getImageUrl(image) {
   const name = image?.img_name;
-  return name
-    ? `http://codeforgreen.zspwrzesnia.pl/plants/database_img/${name}`
-    : 'https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg';
+
+  if (!name) {
+    return 'https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg'; // default
+  }
+
+  if (name.startsWith('http://') || name.startsWith('https://')) {
+    return name;
+  }
+
+  return `http://codeforgreen.zspwrzesnia.pl/plants/database_img/${name}`;
 }
